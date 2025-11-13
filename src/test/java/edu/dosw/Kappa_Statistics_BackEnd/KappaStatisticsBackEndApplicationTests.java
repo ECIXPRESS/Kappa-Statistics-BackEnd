46068d5eb8dcc6c24b2c60b72de4d0197d66558b
Julian54628 <julian.arenas-a@mail.escuelaing.edu.co>
2025-11-12 20:45:47 -0500

    repositorio actualizado con nuevos cambios

---
+++ src/test/java/edu/dosw/Kappa_Statistics_BackEnd/KappaStatisticsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.Kappa_Statistics_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KappaStatisticsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
